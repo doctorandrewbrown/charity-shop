dd1eb66e98e3af44e50df22f04b38d2b3c047bcf
andrew brown <dr.andrew.david.brown@gmail.com>
2024-02-09 20:39:39 +0000

    customises admin view

--- items/admin.py
+++ items/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+# the two classes below extent the admin model to tell admin view what data to show
+
+class ProductAdmin(admin.ModelAdmin):
+    # tuple below
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+# Register your models here. Note admin models defined above are registered alongside respective models
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
